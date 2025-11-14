ca9fbba54376303312e66aec23fb304a2d58d4f4
shinobiultra <f.kucera@media-bias-research.org>
2025-11-14 08:30:15 +0000

    Deploying to gh-pages from @ shinobiultra/shinobiultra.github.io@51fdfb864bdbfacef1b7ded88e646bb0e665e451 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -19,9 +19,9 @@ function determineGiscusTheme() {
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
     "data-repo": "shinobiultra/shinobiultra.github.io",
-    "data-repo-id": "",
+    "data-repo-id": "R_kgDOQUBNHA",
     "data-category": "Comments",
-    "data-category-id": "",
+    "data-category-id": "DIC_kwDOQUBNHM4CxtVR",
     "data-mapping": "title",
     "data-strict": "1",
     "data-reactions-enabled": "1",
